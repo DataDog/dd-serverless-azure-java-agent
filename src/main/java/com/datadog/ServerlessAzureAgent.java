427589120a42c09398fd204df777635b23616870
Duncan Harvey <duncan.harvey@datadoghq.com>
2024-08-21 16:56:05 -0400

    add support for azure functions running on linux and windows

--- src/main/java/com/datadog/ServerlessAzureAgent.java
+++ src/main/java/com/datadog/ServerlessAzureAgent.java
@@ -10,18 +10,47 @@ import org.slf4j.LoggerFactory;
 
 public class ServerlessAzureAgent {
     private static final Logger log = LoggerFactory.getLogger(ServerlessAzureAgent.class);
-    private static final String fileName = "datadog-serverless-trace-mini-agent";
+    private static final String os = System.getProperty("os.name").toLowerCase();
+    private static String fileName = null;
+    private static String tempDirPath = null;
+
+    public static boolean isWindows() {
+        return os.contains("win");
+    }
+
+    public static boolean isLinux() {
+        return os.contains("linux");
+    }
 
     public static void premain(String agentArgs, Instrumentation instrumentation) {
+        if (isWindows()) {
+            log.debug("Detected {}", os);
+            fileName = "datadog-serverless-trace-mini-agent.exe";
+            tempDirPath = "C:/local/Temp/datadog";
+        } else if (isLinux()) {
+            log.debug("Detected {}", os);
+            fileName = "datadog-serverless-trace-mini-agent";
+            tempDirPath = "/tmp/datadog";
+        } else {
+            log.error("Unsupported operating system {}", os);
+            return;
+        }
+
         log.info("Attempting to start {}", fileName);
 
         try (InputStream inputStream = ServerlessAzureAgent.class.getClassLoader()
                 .getResourceAsStream(fileName)) {
             if (inputStream == null) {
                 log.error("{} not found", fileName);
+                return;
             }
 
-            File executableFile = new File(fileName);
+            File tempDir = new File(tempDirPath);
+            if (!tempDir.exists()) {
+                tempDir.mkdirs();
+            }
+
+            File executableFile = new File(tempDir, fileName);
             Files.copy(inputStream, executableFile.getAbsoluteFile().toPath(), StandardCopyOption.REPLACE_EXISTING);
             executableFile.setExecutable(true);
 
